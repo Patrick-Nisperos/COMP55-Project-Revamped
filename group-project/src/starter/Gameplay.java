c5dbd0781268ae8348c798c702df1737aa8cb99a
BryanHerrera19 <b_herrera6@u.pacific.edu>
2020-10-26 10:01:44 -0700

    Added code from old Gameplay class repository to new one.

--- group-project/src/starter/Gameplay.java
+++ group-project/src/starter/Gameplay.java
@@ -1,5 +1,24 @@
 package starter;
 
-public class Gameplay {
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Gameplay extends GraphicsProgram {
+	public static final int PROGRAM_WIDTH = 1600;
+	public static final int PROGRAM_HEIGHT = 900;
+	
+	
+	
+	public void init() {
+		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
+	}
+	
+	public void run() { // Main Function
+		GRect rect = new GRect(20, 20, 50, 40);
+		add(rect);
+	}
 
 }
