4e38cd06022392c3d1f18aa250ff5b2c2f03b8fc
Adil Hajiyev <a_hajiyev@u.pacific.edu>
2020-10-26 21:28:10 +0400

    Created constructor for Enemy.java and added getters and setters.

--- group-project/src/starter/Enemy.java
+++ group-project/src/starter/Enemy.java
@@ -1,9 +1,57 @@
 package starter;
 
 public class Enemy {
- EnemyType typeOfEnemy;
- boolean isDestroyed;
- int cordinateX;
- int cordinateY;
- Projectile enemyProjectile;
+ private EnemyType typeOfEnemy;
+ private boolean isDestroyed;
+ private int cordinateX;
+ private int cordinateY;
+ private Projectile enemyProjectile;
+ 
+ public Enemy (EnemyType typeOfEnemy, boolean isDestroyed , int cordinateX, int cordinateY, Projectile enemyProjectile) {
+		this.typeOfEnemy = typeOfEnemy;
+		this.isDestroyed = isDestroyed;
+		this.cordinateX = cordinateX;
+		this.cordinateY = cordinateY;
+		this.enemyProjectile=enemyProjectile;
+	}
+
+public EnemyType getTypeOfEnemy() {
+	return typeOfEnemy;
+}
+
+public void setTypeOfEnemy(EnemyType typeOfEnemy) {
+	this.typeOfEnemy = typeOfEnemy;
+}
+
+public boolean isDestroyed() {
+	return isDestroyed;
+}
+
+public void setDestroyed(boolean isDestroyed) {
+	this.isDestroyed = isDestroyed;
+}
+
+public int getCordinateX() {
+	return cordinateX;
+}
+
+public void setCordinateX(int cordinateX) {
+	this.cordinateX = cordinateX;
+}
+
+public int getCordinateY() {
+	return cordinateY;
+}
+
+public void setCordinateY(int cordinateY) {
+	this.cordinateY = cordinateY;
+}
+
+public Projectile getEnemyProjectile() {
+	return enemyProjectile;
+}
+
+public void setEnemyProjectile(Projectile enemyProjectile) {
+	this.enemyProjectile = enemyProjectile;
+}
 }
