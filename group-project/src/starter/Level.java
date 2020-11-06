9fee2b9c104a421537c2dd7c7b73cf1ea3db551e
BryanHerrera19 <b_herrera6@u.pacific.edu>
2020-11-06 10:36:06 -0800

    Added base setting for the different enemies for them to be added into an array list.

--- group-project/src/starter/Level.java
+++ group-project/src/starter/Level.java
@@ -4,56 +4,52 @@ import java.util.ArrayList;
 import acm.graphics.*;
 
 public class Level {
+	private int levelNumber;
+	
 	private int numberOfEnemiesLight;
 	private int numberOfEnemiesMedium;
 	private int numberOfEnemiesHeavy;
 	private int numberOfPlayers;
 	
-	private ArrayList<EnemyType> enemies;
+	private ArrayList<Enemy> enemies;
 	private ArrayList<Shield> shields;
 	private ArrayList<CharacterEntity> users;
 	
+	Projectile enemyProjectile = new Projectile(5);
 	Projectile userProjectile = new Projectile(5);
 	Hitbox userHitbox = new Hitbox(50, 70);
 	CharacterEntity user = new CharacterEntity(100, 50, false, 800, 800, userProjectile, userHitbox);
 	
 	Hitbox enemyLightHitbox = new Hitbox(50, 70);
 	EnemyType typeLight = new EnemyType(50, 10, enemyLightHitbox, EnemyTypeCode.LIGHT);
+	Enemy enemyLight = new Enemy(typeLight, false, 100, 100, enemyProjectile);
+	
 	 
 	Hitbox enemyMediumHitbox = new Hitbox(60, 80);
 	EnemyType typeMedium = new EnemyType(100, 20, enemyMediumHitbox, EnemyTypeCode.MEDIUM);
+	Enemy enemyMedium = new Enemy(typeMedium, false, 100, 100, enemyProjectile);
 	 
 	Hitbox enemyHeavyHitbox = new Hitbox(80, 100);
 	EnemyType typeHeavy = new EnemyType(200, 50, enemyHeavyHitbox, EnemyTypeCode.HEAVY);
-	 
-	Projectile enemyProjectile = new Projectile(5);
+	Enemy enemyHeavy = new Enemy(typeHeavy, false, 100, 100, enemyProjectile);
 	
 	Shield gameShield = new Shield(100, 200, 500);
 	
-	public Level(int numberOfEnemiesLight, int numberOfEnemiesMedium, int numberOfEnemiesHeavy, int numberOfPlayers, int numberOfShields) {
-		this.numberOfEnemiesLight = numberOfEnemiesLight;
-		this.numberOfEnemiesMedium = numberOfEnemiesMedium;
-		this.numberOfEnemiesHeavy = numberOfEnemiesHeavy;
-		this.numberOfPlayers = numberOfPlayers;
+	
+	
+	public Level(int levelNumber) {
+		this.setLevelNumber(levelNumber);
+		setNumberOfEnemiesLight(21);
 		
-		for(int i = 0; i <= numberOfEnemiesLight; i++) {
-			enemies.add(typeLight);
-		}
-		for(int i = 0; i <= numberOfEnemiesMedium; i++) {
-			enemies.add(typeMedium);
-		}
-		for(int i = 0; i <= numberOfEnemiesHeavy; i++) {
-			enemies.add(typeHeavy);
-		}
-		
-		for(int i = 0; i <= numberOfPlayers; i++) {
-			users.add(user);
-		}
-		
-		for(int i = 0; i <= numberOfShields; i++) {
-			
+		if(levelNumber == 1) {
+			for(int i = 0; i == 21; i++) {
+				if(i < 8) { //layer one
+					
+				}
+			}
 		}
 	}
+	
 	public void createEnemy() {
 		
 	}
@@ -66,5 +62,46 @@ public class Level {
 	}
 	public void placeScoreSystem() {
 		
+	}
+
+	
+	public int getNumberOfEnemiesLight() {
+		return numberOfEnemiesLight;
+	}
+
+	public void setNumberOfEnemiesLight(int numberOfEnemiesLight) {
+		this.numberOfEnemiesLight = numberOfEnemiesLight;
+	}
+
+	public int getNumberOfEnemiesMedium() {
+		return numberOfEnemiesMedium;
+	}
+
+	public void setNumberOfEnemiesMedium(int numberOfEnemiesMedium) {
+		this.numberOfEnemiesMedium = numberOfEnemiesMedium;
+	}
+
+	public int getNumberOfEnemiesHeavy() {
+		return numberOfEnemiesHeavy;
+	}
+
+	public void setNumberOfEnemiesHeavy(int numberOfEnemiesHeavy) {
+		this.numberOfEnemiesHeavy = numberOfEnemiesHeavy;
+	}
+
+	public int getNumberOfPlayers() {
+		return numberOfPlayers;
+	}
+
+	public void setNumberOfPlayers(int numberOfPlayers) {
+		this.numberOfPlayers = numberOfPlayers;
+	}
+
+	public int getLevelNumber() {
+		return levelNumber;
+	}
+
+	public void setLevelNumber(int levelNumber) {
+		this.levelNumber = levelNumber;
 	}
 }
