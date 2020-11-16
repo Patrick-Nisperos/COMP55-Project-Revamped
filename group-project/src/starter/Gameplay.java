b4fe652aad2f35eb24868c232fb3d6bfc7b547b3
Adil Hajiyev <a_hajiyev@u.pacific.edu>
2020-11-16 22:46:46 +0400

    Tried to add inputlisteners to gameplay, not working yet, Errors might exist

--- group-project/src/starter/Gameplay.java
+++ group-project/src/starter/Gameplay.java
@@ -46,6 +46,22 @@ public class Gameplay extends GraphicsProgram implements ActionListener,KeyListe
 	private Timer controlScreenTimer = new Timer(1000, this);
 	private Timer gameTimer = new Timer(1000, this); //used to track if user is in game
 	
+	 private GImage tank;
+	    private  int tankXcord = 20;
+	    private  int tankYcord = 800 ;
+	    private final int SCREEN_WIDTH = 1600;
+	    private final int SCREEN_HEIGHT = 900;
+	    private int arrIndex = 0;
+	    private int speedX = 0; 
+	    private int speedY = 0;
+	    private int speedVal = 5;
+	    private int x = 0;
+	    private int y = 0;
+	    // below line is in case if we want to add different images to make it look animated
+	    private String[] pics = {"blue tank.png"}; 
+	    //private Timer t = new Timer(25, this);
+	    
+	    
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
 		addKeyListeners();
@@ -129,15 +145,65 @@ public class Gameplay extends GraphicsProgram implements ActionListener,KeyListe
 				System.out.println("You pressed *esc* ");
 				pauseGame();
 			}
+		
+			char moveKey = e.getKeyChar();
+	        int moveKeyCode = e.getKeyCode();
+	        
+	        if(singlePlayerTimer.isRunning()) {
+	        if(moveKey == 'd' || moveKeyCode==KeyEvent.VK_RIGHT){
+	        	speedX=speedVal;
+	        	speedY=0;
+	        		tank.move(speedX,0);
+	                tank.setImage(pics[arrIndex]);
+	                arrIndex++;
+	                if(arrIndex>=pics.length){
+	                    arrIndex = 0;
+	                }
+	            }
+	        else if(moveKey == 'a'|| moveKeyCode==KeyEvent.VK_LEFT) {
+	        	speedX=-speedVal;
+	        	speedY=0;
+	        	tank.move(-speedX,0);
+	               tank.setImage(pics[arrIndex]);
+	               arrIndex++;
+	               if(arrIndex>=pics.length){
+	                   arrIndex = 0;
+	               }
+	        }
+	        else if(moveKey == 'w'|| moveKeyCode==KeyEvent.VK_UP) {
+	        	speedX=0;
+	        	speedY=-speedVal;
+	        	tank.move(0,-speedY);
+	            tank.setImage(pics[arrIndex]);
+	            arrIndex++;
+	            if(arrIndex>=pics.length){
+	                arrIndex = 0;
+	            }
+	     }
+	        else if(moveKey == 's'|| moveKeyCode==KeyEvent.VK_DOWN) {
+	        	speedX=0;
+	        	speedY=speedVal;
+	        	tank.move(0,speedY);
+	             tank.setImage(pics[arrIndex]);
+	             arrIndex++;
+	             if(arrIndex>=pics.length){
+	                 arrIndex = 0;
+	             }
+	      }
+	      
+		}
 		}
 	}
 	
 	public void keyReleased(KeyEvent e) {
 		  System.out.println("You released key code: " + e.getKeyCode());
+			speedX=0;
+	    	speedY=0;
 	}	
 	
 	public void actionPerformed(ActionEvent e) {
 		//Main menu button click recognition
+		
 		if(mainMenuTimer.isRunning()) {
 			if(singlePlayerButtonPressed) {
 				mainMenuTimer.stop();
@@ -184,7 +250,27 @@ public class Gameplay extends GraphicsProgram implements ActionListener,KeyListe
 		}
 		if(singlePlayerTimer.isRunning()) {
 			enemyMovement();
+			if(x<0) {
+	    		speedX=0;
+	    		x=0;
+	    	}
+	    	if(x>SCREEN_WIDTH-90) {
+	    		speedX=0;
+	    		x=SCREEN_WIDTH-90;
+	    	}
+	    	if(y<0) {
+	    		speedY=0;
+	    		y=0;
+	    	}
+	    	if(y>SCREEN_HEIGHT-90) {
+	    		speedY=0;
+	    		y=SCREEN_HEIGHT-90;
+	    	}
+	    	x+=speedX;
+	    	y+=speedY;
+//	    	add(tank,x,y);
 		}
+		
 		if(controlScreenTimer.isRunning()) {
 			if(controlReturnMainMenuPressed) {
 				gameTimer.stop();
@@ -237,6 +323,17 @@ public class Gameplay extends GraphicsProgram implements ActionListener,KeyListe
 		playerShields = levelOne.placeShield();
 		pasteShields();
 		
+		   //setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	        tank = new GImage("blue tank.png");
+	        x=tankXcord;
+	        y=tankYcord;
+	        add(tank,x,y);
+	       // t.start();
+	        //addKeyListeners();
+	        setFocusable(true);
+	        setFocusTraversalKeysEnabled(false);
+	      //  addMouseListeners();
+		//add(tank,x,y);
 		//If level is done or player decides to leave
 //		boolean endLevel = false;
 //		if(endLevel) {
