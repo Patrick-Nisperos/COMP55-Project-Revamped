9f61428b7a95385ecd5d19e6d783c90ff0644a55
Adil Hajiyev <a_hajiyev@u.pacific.edu>
2020-11-18 22:09:16 +0400

    Completed Projectile.java

--- group-project/src/starter/Projectile.java
+++ group-project/src/starter/Projectile.java
@@ -1,29 +1,37 @@
 package starter;
 
+import acm.graphics.*;
+
 public class Projectile {
 	private int speed;
+	private int height;
+	private int width;
+	private GImage projectilePic=new GImage("projectile px.png");
 	
-	public Projectile(int speed) {
+	public Projectile(int speed,int height,int width) {
 		this.speed=speed;
+		this.height=height;
+		this.width=width;
 	}
-	
-	public static void characterProjectile() {
-		
-	}
-	
-	public static void enemyProjectile() {
-		
-	}
+
 	
 	public static Boolean isProjectileDestroyed(Boolean x) {
 		return x;
 	}
-
+	
+	public GImage getProjectilePic() {
+		return projectilePic;
+	}
+	
 	public int getSpeed() {
 		return speed;
 	}
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
+	public void setSize(int height,int width) {
+		projectilePic.setSize(height,width);
+	}
+
 	
 }
