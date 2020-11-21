59fed7da452a5d2d28ede59748dce99ea9a03abf
Patrick <p_nisperos@u.pacific.edu>
2020-11-21 12:53:44 -0800

    Repushed my singlePlayerObjHit code and no unlimited fire.

--- group-project/src/starter/Gameplay.java
+++ group-project/src/starter/Gameplay.java
@@ -175,15 +175,17 @@ public class Gameplay extends GraphicsProgram implements ActionListener,KeyListe
 		            }
 	        	}
 	        }
-	        if(moveKeyCode == 87) { 
-	        	System.out.println("you pressed *w* fire projectile");
-				singlePlayerUserFire();
-	        }	      
+//	        if(moveKeyCode == 87) { 
+//	        	System.out.println("you pressed *w* fire projectile");
+//	
+//	        }	      
 	     }
 	}
 	
 	public void keyReleased(KeyEvent e) {
-		System.out.println("You released key code: " + e.getKeyCode());      
+		System.out.println("You released key code: " + e.getKeyCode()); 
+		System.out.println("you pressed *w* fire projectile");
+		singlePlayerUserFire();
 	}	
 	
 	
@@ -372,15 +374,14 @@ public class Gameplay extends GraphicsProgram implements ActionListener,KeyListe
 		}
 	}
 	public void singlePlayerObjHit() {
-		double coordX,coordY;
-		for(Projectile temp:singlePlayerProjectiles) {
-			coordX=temp.getProjectilePic().getX()+temp.getProjectilePic().getWidth()+1;
-			coordY=temp.getProjectilePic().getY()+(temp.getProjectilePic().getHeight());
+		for(int i = 0; i < singlePlayerProjectiles.size(); i++) {
+			Projectile temp = singlePlayerProjectiles.get(i);
+			double coordX=temp.getProjectilePic().getX()+temp.getProjectilePic().getWidth()+1;
+			double coordY=temp.getProjectilePic().getY()+(temp.getProjectilePic().getHeight()/2);
 			if(getElementAt(coordX,coordY) instanceof GRect) {
-				remove(getElementAt(coordX,coordY));
 				remove(temp.getProjectilePic());
-//				remove(getElementAt(temp.getProjectilePic().getX(),temp.getProjectilePic().getY()));
-//				singlePlayerProjectiles.remove(temp);
+				remove(getElementAt(coordX, coordY));
+				singlePlayerProjectiles.remove(temp);
 			}
 		}
 	}
